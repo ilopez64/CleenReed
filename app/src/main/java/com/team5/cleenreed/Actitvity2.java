b5c51ccd10e5f4e0246d5e6fb52471e82d4c3269
Irving Lopez <ilopez64@protonmail.com>
2019-04-03 14:38:52 -0400

    Adding gmail api

--- app/src/main/java/com/team5/cleenreed/Actitvity2.java
+++ app/src/main/java/com/team5/cleenreed/Actitvity2.java
@@ -2,6 +2,8 @@ package com.team5.cleenreed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.api.services.gmail.Gmail;
+
 
 public class Actitvity2 extends AppCompatActivity {
 
