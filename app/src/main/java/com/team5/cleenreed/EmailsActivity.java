0d7f2532826b1bbc504c17f471763e6e53373456
Irving Lopez <ilopez64@protonmail.com>
2019-04-30 13:40:18 -0400

    more google stuff added

--- app/src/main/java/com/team5/cleenreed/EmailsActivity.java
+++ app/src/main/java/com/team5/cleenreed/EmailsActivity.java
@@ -1,7 +1,6 @@
 package com.team5.cleenreed;
 
 import android.accounts.Account;
-import android.net.wifi.hotspot2.pps.Credential;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,33 +14,7 @@ import java.util.List;
 
 
 //Firebase APIs
-import com.google.android.gms.auth.GoogleAuthUtil;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.api.client.extensions.android.http.AndroidHttp;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.util.Collections2;
-import com.google.api.client.util.ExponentialBackOff;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.api.services.gmail.GmailScopes;
-import javax.mail.*;
 
 //Chaquopy
 import com.chaquo.python.PyObject;
@@ -49,20 +22,26 @@ import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 
 //Gmail imports
+import com.google.android.gms.common.api.ApiException;
+import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.android.gms.auth.api.credentials.CredentialsClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
-
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
+import javax.mail.*;
+import javax.mail.internet.MimeMessage;
+
 
 public class EmailsActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -71,7 +50,7 @@ public class EmailsActivity extends AppCompatActivity {
     private RecyclerAdapter adapter;
 
     /** Global instance of the HTTP transport. */
-    private static HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
+    private static final HttpTransport HTTP_TRANSPORT = AndroidHttp.newCompatibleTransport();
     /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
@@ -81,10 +60,7 @@ public class EmailsActivity extends AppCompatActivity {
     private static final String[] SCOPES = {GmailScopes.GMAIL_READONLY,GmailScopes.GMAIL_MODIFY,GmailScopes.GMAIL_METADATA};
     private static final String TAG = "CleenReed";
     private static final int RC_SIGN_IN = 9001;
-    private GoogleApiClient mGoogleApiClient;
-    private GoogleSignInOptions gso;
-    GoogleSignInClient mGoogleSignInClient;
-
+    private GoogleAccountCredential mCredential;
 
 
     @Override
@@ -92,17 +68,37 @@ public class EmailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emails);
 
+        // Get last Signed in Account From Main Activity Sign in
         mAccount = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
         email = mAccount.getEmail();
 
+        Gmail
+
+        // Initialize credentials and service object.
+        mCredential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff());
 
 
-        //Gmail gmail = new Gmail.Builder(HTTP_TRANSPORT,JSON_FACTORY, credential).setApplicationName(TAG).build();
+        // Create Gmail service object
+        Gmail gmail = new Gmail.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, mCredential)
+                .setApplicationName(TAG)
+                .build();
 
-        List<String> list2;
+        //String user = "me";
+
+        //getLabel(service,email,"IMPORTANT");
+        List<String> labels = new ArrayList<>();
+        labels.add("IMPORTANT");
+
+        List<Message> messages = new ArrayList<>();
+        //messages = listMessagesWithLabels(gmail,user,labels);
+
         ArrayList<String> list = new ArrayList<>();
         list.add(email);
 
+        /* Set Recycler View to Emails Item List */
         recyclerView = findViewById(R.id.recyclerview);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
@@ -110,16 +106,6 @@ public class EmailsActivity extends AppCompatActivity {
         adapter = new RecyclerAdapter(list);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
-
-
-
-        //getLabel(service,email,"IMPORTANT");
-        List<String> labels = new ArrayList<>();
-        labels.add("IMPORTANT");
-
-        //Message message = service.users().messages().list("me").setLabelIds(labels).execute();
-        List<Message> messages;
-        //messages = listMessagesWithLabels(service,email,labels);
 
     }
     @Override
